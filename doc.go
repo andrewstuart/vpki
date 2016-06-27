4650e8e60cd9878ac7fe1879653dfcdbf4ce3f0e
Andrew Stuart <andrew.stuart2@gmail.com>
2016-06-27 12:58:12 -0700

    Update documentation

--- doc.go
+++ doc.go
@@ -1,5 +1,6 @@
 // Package vtls provides a layer of abstraction between the golang stdlib
 // crypto primitives and common crypto uses (e.g. serving HTTPS) and the
-// functionality provided by Vault. Internally, the library generates RSA keys
-// locally
+// functionality provided by Vault. Internally, the library generates private
+// keys locally and sends CSRs to the vault server, so that private keys are
+// never transmitted.
 package vtls
